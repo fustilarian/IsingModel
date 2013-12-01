82842f48861038fbc3111813f2fc24105be5e7ff
Henry O'Hagan <henryohagan@henrys-air.lan>
2013-12-01 18:17:43 +0000

    Initial Commit

---
+++ ising/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ising
+//
+//  Created by Henry O'Hagan on 01/12/2013.
+//  Copyright (c) 2013 Henry O'Hagan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
